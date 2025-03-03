6c394ec23ac41683b0185ca9bfcf9751466c754d
viniciusalves226 <viniciusalves@eaportal.org>
2025-03-03 15:22:47 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+var primeiro = parseInt(prompt("Digite primeiro numero"));
+var segundo = parseInt(prompt("Digite um segundo numero"));
+var resultado = primeiro + segundo;
+console.log(primeiro + " + " + segundo + " = " + resultado );
